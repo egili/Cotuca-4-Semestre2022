20679c50cb74660b0fae4b689030c2a9f33bf9b1
eliseu gili <gili.eliseup@gmail.com>
2022-09-10 21:50:15 -0300

    data annotations

--- Desenvolvimento Web III/escola/ProjetoEscola_API/Models/Aluno.cs
+++ Desenvolvimento Web III/escola/ProjetoEscola_API/Models/Aluno.cs
@@ -1,10 +1,21 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace ProjetoEscola_API.Models
 {
     public class Aluno
     {
-        public int id       {get; set;} //entity framework, toda tabela precisa ter o campo id
+        public int id       {get; set;} // entity framework, toda tabela precisa ter o campo id
+        
+        [Required]
+        [StringLength(5)]
         public string? ra   {get; set;}
+
+        [Required]
+        [StringLength(30)]
         public string? nome {get; set;}
+
+        [Required]
+        [StringLength(2)]
         public int codCurso {get; set;}
     }
 }

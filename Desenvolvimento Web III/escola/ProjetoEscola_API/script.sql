df8563ecebe94af12bc22d5c32f4990173f9c12a
Eliseu Gili <gili.eliseup@gmail.com>
2022-09-23 20:45:26 -0300

    script

--- Desenvolvimento Web III/escola/ProjetoEscola_API/script.sql
+++ Desenvolvimento Web III/escola/ProjetoEscola_API/script.sql
@@ -9,4 +9,17 @@ Insert into Aluno (ra,nome,codCurso) values ('20001','Paulo',19)
 Insert into Aluno (ra,nome,codCurso) values ('20002','Ana',19)
 Insert into Aluno (ra,nome,codCurso) values ('20003','Pedro',15)
 
+
+CREATE TABLE Curso (
+    [id]       INT          IDENTITY (1, 1) NOT NULL,
+    [codCurso] INT          NULL,
+    [nomeCurso]     VARCHAR (30) NULL,
+    [periodo]       VARCHAR (1) NULL,
+    PRIMARY KEY CLUSTERED ([id] ASC)
+);
+
+Insert into Curso (codCurso, nomeCurso, periodo) values (19,'Informática', 'M')
+Insert into Curso (codCurso, nomeCurso, periodo) values (39,'Desenvolvimento para Internet', 'V')
+Insert into Curso (codCurso, nomeCurso, periodo) values (59,'Desenvolvimento para Internet', 'N')
+
 --sql server
